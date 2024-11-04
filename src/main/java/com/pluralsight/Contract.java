5bb574435c785df7e33b55b9212adab2206979b0
Ekrem <ealp@pgh.yearup.org>
2024-11-04 15:30:23 -0500

    Implemented Contract class with common properties for sales and leases

--- src/main/java/com/pluralsight/Contract.java
+++ src/main/java/com/pluralsight/Contract.java
@@ -1,4 +1,34 @@
 package com.pluralsight;
 
-public class Contract {
+public abstract class Contract {
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private String vehicleSold;
+
+    public Contract(String date, String customerName, String customerEmail, String vehicleSold) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public String getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public abstract double getTotalPrice();
+    public abstract double getMonthlyPayment();
 }
