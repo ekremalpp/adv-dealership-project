0ff3e8caf01e510d7855e60ecfd5ea224475b7b1
Ekrem <ealp@pgh.yearup.org>
2024-11-05 20:42:07 -0500

    uploaded ContractFileManegerClass

--- src/main/java/com/pluralsight/ContractFileManager.java
+++ src/main/java/com/pluralsight/ContractFileManager.java
@@ -1,4 +1,22 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ContractFileManager {
+
+    private String fileName = "contracts.csv";
+
+    public void saveContract(Contract contract) {
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName, true));
+            bufferedWriter.write(contract.toString());
+            bufferedWriter.newLine();
+            bufferedWriter.close();
+            System.out.println("Contract saved successfully to " + fileName);
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file.");
+        }
+    }
 }
